a018ece52837cb4a6a6a9c3fce40dd59253806dd
lucasmercier <pro@lucasmercier.com>
2024-04-29 10:23:35 +0200

    initial commit

---
+++ src/main/java/fr/chatop/api/ChaTopApiBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.chatop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaTopApiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaTopApiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/chatop/api/ChaTopApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.chatop.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaTopApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
