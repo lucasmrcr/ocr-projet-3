8154d00f98c283654a2f4f7a0df48fa3c90dca7e
lucasmercier <114911557+lucasmercierredlab@users.noreply.github.com>
2024-04-30 11:27:46 +0200

    add swagger doc

--- src/main/java/fr/chatop/api/controller/MessageController.java
+++ src/main/java/fr/chatop/api/controller/MessageController.java
@@ -3,6 +3,11 @@ package fr.chatop.api.controller;
 import fr.chatop.api.dto.request.message.CreateMessageDTO;
 import fr.chatop.api.dto.response.MessageDTO;
 import fr.chatop.api.services.IMessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +22,14 @@ public class MessageController {
 
     private final IMessageService messageService;
 
+    @Operation(
+        summary = "Send message",
+        description = "Send a message"
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Message send with success", content = @Content(schema = @Schema(implementation = MessageDTO.class))),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+    })
     @PostMapping
     public ResponseEntity<MessageDTO> sendMessage(@RequestBody CreateMessageDTO createMessage) {
         messageService.sendMessage(createMessage);
--- src/main/java/fr/chatop/api/controller/PictureController.java
+++ src/main/java/fr/chatop/api/controller/PictureController.java
@@ -1,6 +1,7 @@
 package fr.chatop.api.controller;
 
 import fr.chatop.api.services.IPictureService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,10 @@ public class PictureController {
 
     private final IPictureService pictureService;
 
+    @Operation(
+        summary = "Get picture",
+        description = "Retrieve picture by id"
+    )
     @GetMapping(value = "/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
     public @ResponseBody byte[] getPicture(@PathVariable String id) {
         return pictureService.getPicture(id);
